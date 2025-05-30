4bb77de5e9048d0a1085d9dbc131903a4d261e33
endsun1234 <endsun1234@naver.com>
2025-05-30 16:52:01 +0900

    2025.05.30

---
+++ Assets/02_Scripts/UIUX/CoolTimeSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolTimeSystem : MonoBehaviour
+{
+    public float curTime;
+    public float coolTime = 2f;
+    public bool isClicked = false;
+    public Image _image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image.fillAmount = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isClicked)
+        {
+            curTime += Time.deltaTime;
+            _image.fillAmount = curTime / coolTime;
+            if (curTime >= coolTime)
+            {
+                curTime = 0f;
+                isClicked = false;
+            }
+        }
+    }
+
+    public void OnclickStartBtn()
+    {
+        isClicked = true;
+    }
+}
